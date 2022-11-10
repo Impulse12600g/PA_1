788f376bd4e666579a54c4e491fc96dfd80e28c1
Jmansito <j_mansito@icloud.com>
2022-11-09 21:34:11 -0700

    Project updated / requirements met

--- src/main/java/edu/ucdenver/tournament/Team.java
+++ src/main/java/edu/ucdenver/tournament/Team.java
@@ -23,8 +23,11 @@ public class Team implements Serializable {
         else return new ArrayList<>(this.squad);
     }
 
+    // Add player to national squad, can't have more than 35 on a team
     public void addPlayer(String name, int age, double height, double weight){
-        this.squad.add(new Player(name, age, height, weight));
+        if(this.getSquad() == null) this.squad.add(new Player(name, age, height, weight));
+        else if(this.getSquad().size() < 35) this.squad.add(new Player(name, age, height, weight));
+        else throw new IllegalArgumentException("Squad already has 35 players");
     }
     public String getName() {
         return name;
